4f92c8aa07ccd07f26140795da8380e06ad22ff3
Naomi <naomina1836@gmail.com>
2026-08-03 19:10:30 +0000

    updated main

--- hrm-frontend/src/main.jsx
+++ hrm-frontend/src/main.jsx
@@ -8,7 +8,12 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter
+            future={{
+                v7_relativeSplatPath: true,
+                v7_startTransition: true,
+            }}
+        >
             <AuthProvider>
                 <App />
                 <Toaster
